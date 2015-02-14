22275323fde2f9f6461485ff3bc01f7006c27c49
Diga Widyaprana <digawp@gmail.com>
2015-02-14 21:10:58 +0800

    Fix bugs and add docs

--- FileReceiver.java
+++ FileReceiver.java
@@ -1,4 +1,4 @@
-// Author: 
+// Author: A0114171W
 
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
@@ -7,67 +7,70 @@ import java.net.*;
 import java.nio.ByteBuffer;
 
 class FileReceiver {
-    
-    public DatagramSocket socket; 
+
+    public DatagramSocket socket;
     public DatagramPacket pkt;
     int portNo;
-    
+
     public static void main(String[] args) {
-        
         // check if the number of command line argument is 1
         if (args.length != 1) {
             System.out.println("Usage: java FileReceiver port");
             System.exit(1);
         }
-        
+
         FileReceiver receiver = new FileReceiver(args[0]);
-        
+
         try {
-			receiver.receive();
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+            receiver.receive();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
-    
+
     public FileReceiver(String localPort) {
         portNo = Integer.parseInt(localPort);
     }
-    
+
     void receive() throws IOException {
-    	byte[] inBuffer = new byte[1000];
-    	socket = new DatagramSocket(portNo);
-    	pkt = new DatagramPacket(inBuffer, inBuffer.length);
-    	
-    	socket.receive(pkt);
-		
-		ByteBuffer wrapper = ByteBuffer.wrap(pkt.getData(), 0, 4);
-		int nameLength = wrapper.getInt();
-		String fileName = new String(pkt.getData(), 4, nameLength + 4);
-		
-		FileOutputStream fos = new FileOutputStream(fileName);
-		BufferedOutputStream bos = new BufferedOutputStream(fos);
-		
-    	while (true) {
-    		socket.receive(pkt);
-    		wrapper = ByteBuffer.wrap(pkt.getData(), 0, 4);
-    		nameLength = wrapper.getInt();
-    		fileName = new String(pkt.getData(), 4, nameLength + 4);
-    		
-    		int dataOffset = nameLength + 4;
-    		
-    		wrapper = ByteBuffer.wrap(pkt.getData(), dataOffset, dataOffset + 4);
-    		int dataLength = wrapper.getInt();
-    		
-    		wrapper = ByteBuffer.wrap(pkt.getData(), dataOffset + 4, dataOffset + 4 + dataLength); 
-    		byte[] data = new byte[dataLength];
-    		wrapper.get(data);
-    		bos.write(data);
-    		if (nameLength + dataLength + 8 < 1000) {
-				break;
-			}
-    	}
-    	bos.close();
+        byte[] inBuffer = new byte[1000];
+        socket = new DatagramSocket(portNo);
+        pkt = new DatagramPacket(inBuffer, inBuffer.length);
+
+        while (true) {
+            socket.receive(pkt);
+
+            // Get length of file names
+            ByteBuffer wrapper = ByteBuffer.wrap(pkt.getData(), 0, 4);
+            int nameLength = wrapper.getInt();
+
+            // Get file name
+            String fileName = new String(pkt.getData(), 4, nameLength);
+
+            int dataOffset = nameLength + 4;
+
+            FileOutputStream fos = new FileOutputStream(fileName, true);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            // Get length of data payload
+            wrapper = ByteBuffer.wrap(pkt.getData(), dataOffset, 4);
+            int dataLength = wrapper.getInt();
+
+            // Get the data payload
+            wrapper = ByteBuffer.wrap(pkt.getData(), dataOffset + 4, dataLength);
+            byte[] data = new byte[dataLength];
+            wrapper.get(data);
+
+            // Write the data to disk
+            bos.write(data);
+            bos.close();
+
+            // If the packet is not full, it is the last packet.
+            if (nameLength + dataLength + 8 < 1000) {
+                System.out.println(fileName + " received.");
+                break;
+            }
+        }
     }
-    
 }
--- FileSender.java
+++ FileSender.java
@@ -1,4 +1,4 @@
-// Author: 
+// Author: A0114171W
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
@@ -7,83 +7,124 @@ import java.net.*;
 import java.nio.ByteBuffer;
 
 class FileSender {
-    
+
     String srcFileName, hostname, destFileName;
     int portNo;
-    
+
     public static void main(String[] args) {
-        
+
         // check if the number of command line argument is 4
         if (args.length != 4) {
             System.out.println("Usage: java FileSender <path/filename> "
-                                   + "<rcvHostName> <rcvPort> <rcvFileName>");
+                    + "<rcvHostName> <rcvPort> <rcvFileName>");
             System.exit(1);
         }
-        
+
         FileSender sender = new FileSender(args[0], args[1], args[2], args[3]);
-        
+
         try {
-			sender.send();
-			System.out.println(args[0] + " is successfully sent as " + args[3]);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+            sender.send();
+            System.out.println(args[0] + " is successfully sent as " + args[3]);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
-    
-    public FileSender(String fileToOpen, String host, String port, String rcvFileName) {
+
+    public FileSender(String fileToOpen, String host, String port,
+            String rcvFileName) {
         srcFileName = fileToOpen;
         hostname = host;
         portNo = Integer.parseInt(port);
         destFileName = rcvFileName;
-        
-        // UDP transmission is unreliable. Sender may overrun
-        // receiver if sending too fast, giving packet lost as a result.
-        // In that sense, sender may need to pause once in a while.
-        // E.g. Thread.sleep(1); // pause for 1 millisecond
     }
-    
-    void send() throws IOException {
-    	InetAddress rcvAddress = InetAddress.getByName(hostname);
-    	DatagramSocket socket = new DatagramSocket(portNo, rcvAddress);
-		
-		byte[] sendBuffer = new byte[1000];
-		
-		int nameLength = allocateHeaderForFileName(sendBuffer, destFileName);
-		
-		int dataOffset = nameLength;
-		int lengthForData = sendBuffer.length - nameLength - 4;
-		
-		byte[] dataBuffer = new byte[lengthForData];
-		FileInputStream fis = new FileInputStream(srcFileName);
-		BufferedInputStream bis = new BufferedInputStream(fis);
-		
-		int bytesRead;
-		while ((bytesRead = bis.read(dataBuffer)) > 0) {
-			byte[] bytesReadInByte = ByteBuffer.allocate(4).putInt(nameLength).array();
-			System.arraycopy(bytesReadInByte, 0, sendBuffer, dataOffset, 4);
-			System.arraycopy(dataBuffer, 0, sendBuffer, dataOffset + 4, bytesRead);
-			DatagramPacket pkt =
-					new DatagramPacket(sendBuffer, sendBuffer.length, rcvAddress, portNo);
-			socket.send(pkt);
-		}
-		socket.close();
-		bis.close();
+
+    /**
+     * Packet format: 4 parts. First 4 bytes indicate length of file name,
+     * followed by the file name. Third part is another 4 bytes indicating the
+     * length of payload, followed by the payload.
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    void send() throws IOException, InterruptedException {
+        InetAddress rcvAddress = InetAddress.getByName(hostname);
+        DatagramSocket socket = new DatagramSocket();
+
+        byte[] outBuffer = new byte[1000];
+
+        int nameLength = allocateHeaderForFileName(outBuffer, destFileName);
+
+        int dataOffset = nameLength;
+        int lengthForData = outBuffer.length - nameLength - 4;
+
+        byte[] dataBuffer = new byte[lengthForData];
+        FileInputStream fis = new FileInputStream(srcFileName);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+
+        int bytesRead;
+        while ((bytesRead = bis.read(dataBuffer)) > 0) {
+            putDataToBuffer(outBuffer, dataOffset, dataBuffer, bytesRead);
+
+            DatagramPacket pkt = new DatagramPacket(outBuffer,
+                    outBuffer.length, rcvAddress, portNo);
+            socket.send(pkt);
+            Thread.sleep(10);
+        }
+
+        // For corner case where
+        // the size of last packet == size of array available for data,
+        // which may result in receiver to fail to terminate
+        if (bytesRead == lengthForData) {
+            putDataToBuffer(outBuffer, dataOffset, dataBuffer, 0);
+
+            DatagramPacket pkt = new DatagramPacket(outBuffer,
+                    outBuffer.length, rcvAddress, portNo);
+            socket.send(pkt);
+        }
+
+        socket.close();
+        bis.close();
+    }
+
+    /**
+     * Puts data in dataBuffer to outBuffer with from index dataOffset for
+     * bytesRead byte
+     *
+     * @param outBuffer
+     *            The buffer to put the data into
+     * @param dataOffset
+     *            The offset to put the data into
+     * @param dataBuffer
+     *            The data to be put
+     * @param bytesRead
+     *            The amount of data to be put
+     */
+    private void putDataToBuffer(byte[] outBuffer, int dataOffset,
+            byte[] dataBuffer, int bytesRead) {
+        byte[] bytesReadInByte = ByteBuffer.allocate(4).putInt(bytesRead)
+                .array();
+        System.arraycopy(bytesReadInByte, 0, outBuffer, dataOffset, 4);
+        System.arraycopy(dataBuffer, 0, outBuffer, dataOffset + 4, bytesRead);
     }
 
     /**
      * Inserts the packet header for file name
-     * @param sendBuffer The array buffer to be sent
-     * @param fileName The name of the file
+     *
+     * @param sendBuffer
+     *            The array buffer to be sent
+     * @param fileName
+     *            The name of the file
      * @return the number of bytes used for file name header
      */
-	private int allocateHeaderForFileName(byte[] sendBuffer, String fileName) {
-		byte[] fileNameInByte = destFileName.getBytes();
-		int nameLength = fileNameInByte.length;
-		byte[] nameLengthInByte = ByteBuffer.allocate(4).putInt(nameLength).array();
-		
-		System.arraycopy(nameLengthInByte, 0, sendBuffer, 0, 4);
-		System.arraycopy(fileNameInByte, 0, sendBuffer, 4, nameLength);
-		return nameLength + 4;
-	}
+    private int allocateHeaderForFileName(byte[] sendBuffer, String fileName) {
+        byte[] fileNameInByte = destFileName.getBytes();
+        int nameLength = fileNameInByte.length;
+        byte[] nameLengthInByte = ByteBuffer.allocate(4).putInt(nameLength)
+                .array();
+
+        System.arraycopy(nameLengthInByte, 0, sendBuffer, 0, 4);
+        System.arraycopy(fileNameInByte, 0, sendBuffer, 4, nameLength);
+        return nameLength + 4;
+    }
 }
