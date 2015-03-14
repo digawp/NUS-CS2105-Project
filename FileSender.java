e36629f39fcd2386a7dff83c4145e106598a2c9d
Diga Widyaprana <digawp@gmail.com>
2015-03-14 23:22:53 +0800

    FileSender rdt 2.1

--- FileSender.java
+++ FileSender.java
@@ -10,6 +10,7 @@ class FileSender {
 
     String srcFileName, hostname, destFileName;
     int portNo;
+    PacketHandler handler;
 
     public static void main(String[] args) {
 
@@ -20,11 +21,18 @@ class FileSender {
             System.exit(1);
         }
 
-        FileSender sender = new FileSender(args[0], args[1], args[2], args[3]);
+        FileSender sender = new FileSender(args[0], "localhost", args[1], args[2]);
 
         try {
+        	Stopwatch stopwatch = new Stopwatch();
+        	stopwatch.start();
             sender.send();
-            System.out.println(args[0] + " is successfully sent as " + args[3]);
+            stopwatch.stop();
+            System.out.println(args[0] + " is successfully sent as " + args[2]);
+            long minutes = stopwatch.getElapsedTimeSecs() / 60;
+            long seconds = stopwatch.getElapsedTimeSecs() % 60;
+            System.out.println("Elapsed: " + minutes + " minutes "
+            			+ seconds + " seconds.");
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             System.exit(1);
@@ -37,6 +45,7 @@ class FileSender {
         hostname = host;
         portNo = Integer.parseInt(port);
         destFileName = rcvFileName;
+        handler = new PacketHandler();
     }
 
     /**
@@ -51,80 +60,63 @@ class FileSender {
         InetAddress rcvAddress = InetAddress.getByName(hostname);
         DatagramSocket socket = new DatagramSocket();
 
-        byte[] outBuffer = new byte[1000];
+        setupConnection(rcvAddress, socket, destFileName);
 
-        int nameLength = allocateHeaderForFileName(outBuffer, destFileName);
+        byte[] dataBuffer = new byte[PacketHandler.MAX_PAYLOAD_LENGTH];
 
-        int dataOffset = nameLength;
-        int lengthForData = outBuffer.length - nameLength - 4;
-
-        byte[] dataBuffer = new byte[lengthForData];
         FileInputStream fis = new FileInputStream(srcFileName);
         BufferedInputStream bis = new BufferedInputStream(fis);
 
         int bytesRead;
         while ((bytesRead = bis.read(dataBuffer)) > 0) {
-            putDataToBuffer(outBuffer, dataOffset, dataBuffer, bytesRead);
+        	// TODO: change seqNo and ackNo!
+            byte[] outBuffer =
+            		handler.createOutgoingPacket(dataBuffer, bytesRead, 0, 0);
+            DatagramPacket pktOut =
+            		new DatagramPacket(outBuffer, outBuffer.length, rcvAddress, portNo);
 
-            DatagramPacket pkt = new DatagramPacket(outBuffer,
-                    outBuffer.length, rcvAddress, portNo);
-            socket.send(pkt);
-            Thread.sleep(10);
+            byte[] inBuffer = new byte[1000];
+            DatagramPacket pktIn = new DatagramPacket(inBuffer, inBuffer.length);
+
+            socket.send(pktOut);
+            socket.receive(pktIn);
+
+         // TODO: change ackNo!!!
+ 			while (!handler.isGood(pktIn.getData()) ||
+ 					!handler.isCorrectAck(pktIn.getData(), 0)) {
+ 				socket.send(pktOut);
+ 				socket.receive(pktIn);
+ 			}
+            Thread.sleep(100);
         }
-
-        // For corner case where
-        // the size of last packet == size of array available for data,
-        // which may result in receiver to fail to terminate
-        if (bytesRead == lengthForData) {
-            putDataToBuffer(outBuffer, dataOffset, dataBuffer, 0);
-
-            DatagramPacket pkt = new DatagramPacket(outBuffer,
-                    outBuffer.length, rcvAddress, portNo);
-            socket.send(pkt);
-        }
-
         socket.close();
         bis.close();
     }
 
-    /**
-     * Puts data in dataBuffer to outBuffer with from index dataOffset for
-     * bytesRead byte
-     *
-     * @param outBuffer
-     *            The buffer to put the data into
-     * @param dataOffset
-     *            The offset to put the data into
-     * @param dataBuffer
-     *            The data to be put
-     * @param bytesRead
-     *            The amount of data to be put
-     */
-    private void putDataToBuffer(byte[] outBuffer, int dataOffset,
-            byte[] dataBuffer, int bytesRead) {
-        byte[] bytesReadInByte = ByteBuffer.allocate(4).putInt(bytesRead)
-                .array();
-        System.arraycopy(bytesReadInByte, 0, outBuffer, dataOffset, 4);
-        System.arraycopy(dataBuffer, 0, outBuffer, dataOffset + 4, bytesRead);
-    }
+	private void setupConnection(InetAddress rcvAddress, DatagramSocket socket,
+			String fileName) {
+		byte[] data = fileName.getBytes();
 
-    /**
-     * Inserts the packet header for file name
-     *
-     * @param sendBuffer
-     *            The array buffer to be sent
-     * @param fileName
-     *            The name of the file
-     * @return the number of bytes used for file name header
-     */
-    private int allocateHeaderForFileName(byte[] sendBuffer, String fileName) {
-        byte[] fileNameInByte = destFileName.getBytes();
-        int nameLength = fileNameInByte.length;
-        byte[] nameLengthInByte = ByteBuffer.allocate(4).putInt(nameLength)
-                .array();
+		// TODO: change seqNo!
+		byte[] outBuffer =
+				handler.createOutgoingPacket(data, data.length, 0, 0);
+		DatagramPacket pktOut =
+				new DatagramPacket(outBuffer, outBuffer.length, rcvAddress, portNo);
 
-        System.arraycopy(nameLengthInByte, 0, sendBuffer, 0, 4);
-        System.arraycopy(fileNameInByte, 0, sendBuffer, 4, nameLength);
-        return nameLength + 4;
-    }
+		byte[] inBuffer = new byte[1000];
+		DatagramPacket pktIn = new DatagramPacket(inBuffer, inBuffer.length);
+
+		try {
+			socket.send(pktOut);
+			socket.receive(pktIn);
+			// TODO: change ackNo!!!
+			while (!handler.isGood(pktIn.getData()) ||
+					!handler.isCorrectAck(pktIn.getData(), 0)) {
+				socket.send(pktOut);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
