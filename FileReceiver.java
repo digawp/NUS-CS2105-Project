3ad09e80f52810393f602b25433020efd57ebf30
Diga Widyaprana <digawp@gmail.com>
2015-03-15 11:23:28 +0800

    FileReceiver rdt2.1

--- FileReceiver.java
+++ FileReceiver.java
@@ -3,14 +3,14 @@
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.*;
-import java.nio.ByteBuffer;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 class FileReceiver {
 
-    public DatagramSocket socket;
-    public DatagramPacket pkt;
     int portNo;
+    PacketHandler handler;
 
     public static void main(String[] args) {
         // check if the number of command line argument is 1
@@ -31,46 +31,57 @@ class FileReceiver {
 
     public FileReceiver(String localPort) {
         portNo = Integer.parseInt(localPort);
+        handler = new PacketHandler();
     }
 
     void receive() throws IOException {
+    	InetAddress ipAddress = InetAddress.getByName("localhost");
+        DatagramSocket socket = new DatagramSocket(portNo);
+
         byte[] inBuffer = new byte[1000];
-        socket = new DatagramSocket(portNo);
-        pkt = new DatagramPacket(inBuffer, inBuffer.length);
+        DatagramPacket pktIn = new DatagramPacket(inBuffer, inBuffer.length);
 
+        byte[] outBuffer = handler.createOutgoingPacket(null, 0, 0, 0);
+        DatagramPacket pktOut =
+        		new DatagramPacket(outBuffer, outBuffer.length, ipAddress, portNo);
+
+        socket.receive(pktIn);
+        while (!handler.isGood(pktIn.getData())) {
+				socket.send(pktOut);
+				socket.receive(pktIn);
+			}
+
+        byte[] packet = pktIn.getData();
+        String fileName = new String(handler.getPayload(packet));
+
+        FileOutputStream fos = new FileOutputStream(fileName);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+        int ackNo = handler.getSeqNo(packet);
+        outBuffer = handler.createOutgoingPacket(null, 0, 0, ackNo);
+        pktOut = new DatagramPacket(outBuffer, outBuffer.length, ipAddress, portNo);
+
+        // TODO work on this
         while (true) {
-            socket.receive(pkt);
 
-            // Get length of file names
-            ByteBuffer wrapper = ByteBuffer.wrap(pkt.getData(), 0, 4);
-            int nameLength = wrapper.getInt();
-
-            // Get file name
-            String fileName = new String(pkt.getData(), 4, nameLength);
-
-            int dataOffset = nameLength + 4;
-
-            FileOutputStream fos = new FileOutputStream(fileName, true);
-            BufferedOutputStream bos = new BufferedOutputStream(fos);
-
-            // Get length of data payload
-            wrapper = ByteBuffer.wrap(pkt.getData(), dataOffset, 4);
-            int dataLength = wrapper.getInt();
-
-            // Get the data payload
-            wrapper = ByteBuffer.wrap(pkt.getData(), dataOffset + 4, dataLength);
-            byte[] data = new byte[dataLength];
-            wrapper.get(data);
-
+        	socket.send(pktOut);
+        	socket.receive(pktIn);
+        	while (!handler.isGood(pktIn.getData())) {
+        		socket.send(pktOut);
+            	socket.receive(pktIn);
+			}
+        	packet = pktIn.getData();
             // Write the data to disk
+        	byte[] data = handler.getPayload(packet);
             bos.write(data);
-            bos.close();
 
-            // If the packet is not full, it is the last packet.
-            if (nameLength + dataLength + 8 < 1000) {
+            // TODO: If last packet.
+            if (data.length < PacketHandler.MAX_PAYLOAD_LENGTH) {
                 System.out.println(fileName + " received.");
                 break;
             }
         }
+        socket.close();
+        bos.close();
     }
 }
